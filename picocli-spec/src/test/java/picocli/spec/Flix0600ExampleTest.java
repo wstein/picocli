55ceb3ba733a4b6b68e0cadbd6466af0ba472726
Werner Stein <werner.stein@gmail.com>
2026-09-21 02:06:19 +0200

    fix(spec): update flix examples to use Path type for positional files and output option

--- picocli-spec/src/test/java/picocli/spec/Flix0600ExampleTest.java
+++ picocli-spec/src/test/java/picocli/spec/Flix0600ExampleTest.java
@@ -120,7 +120,6 @@ public class Flix0600ExampleTest {
 
         ParseResult build = result.subcommand();
         assertEquals(1, build.matchedPositionals().size());
-        File[] files = build.matchedPositionalValue(0, new File[0]);
         java.nio.file.Path[] files = build.matchedPositionalValue(0, new java.nio.file.Path[0]);
         assertEquals(3, files.length);
     }
